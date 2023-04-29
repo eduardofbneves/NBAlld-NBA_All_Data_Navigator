710c0f050a1d51e5dfaff412b36fb26b7823ae55
Eduardo Nevess <edu.seven2000@gmail.com>
2023-04-29 19:17:49 +0100

    callback

--- dash_app/src/pages/teams.py
+++ dash_app/src/pages/teams.py
@@ -1,12 +1,27 @@
 from dash import register_page, html
+import dash_core_components as dcc
+import pandas as pd
 
 from utils.loader import load_dataframe
+from components.dropdown_stats import dropdown_stats
 
-register_page(__name__, path_template='/<team_id>')
+register_page(__name__, path_template='/teams/<team_id>')
+
+'''
+team = load_dataframe('team')
+ids = team['id'].to_list()
+'''
 
 def layout(team_id = None):
+    team = load_dataframe('team')
+    ids = team['id'].to_list()
+    team_id = int(team_id)
+    if team_id not in ids:
+        return dcc.Location(pathname="/not_found_404")
+        # TODO ver se os ids na team devem estar em str para nao dar erro
+    team = team[team['id'] == team_id]
     return html.Div(
         children = [
-            f"The user requested report ID: {team_id}."
+            dropdown_stats(team, 'Dropdown test', 'drop_test', 'some')
         ]
     )
